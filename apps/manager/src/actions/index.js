63dedf7c3b279322bad76bf94a16da54b83d90de
C. Jordan Ball III <cjordanball@gmail.com>
2017-07-10 08:32:38 -0400

    adding redux thunk to project

--- apps/manager/src/actions/index.js
+++ apps/manager/src/actions/index.js
@@ -1,3 +1,4 @@
+import firebase from 'firebase';
 import Actions from './types';
 
 export const emailChanged = (text) => {
@@ -5,6 +6,29 @@ export const emailChanged = (text) => {
     return {
       type: Actions.EMAIL_CHANGED,
       payload: text
+    };
+};
+
+export const passwordChanged = (text) => {
+  console.log('p', text);
+  return {
+    type: Actions.PASSWORD_CHANGED,
+    payload: text
+  };
+};
+
+export const loginUser = ({ email, password }) => {
+    const reqPromise = firebase.auth().signInWithEmailAndPassword(email, password);
+
+    return (dispatch) => {
+        reqPromise.then((user) => {
+            dispatch(
+                {
+                    type: Actions.SIGN_IN_USER,
+                    payload: user
+                }
+            );
+        });
     };
 };
 
--- apps/manager/src/components/LoginForm.js
+++ apps/manager/src/components/LoginForm.js
@@ -1,42 +1,57 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
 import { connect } from 'react-redux';
 import { Card, CardSection, Input, Button } from './common';
 import * as actions from '../actions';
 
 class LoginForm extends Component {
+	onEmailChange(text) {
+		this.props.emailChanged(text);
+	}
 
-  onEmailChange(text) {
-    console.log('onE', this.props.email);
-    this.props.emailChanged(text);
-  }
-  render() {
-    return (
-      <Card>
-        <CardSection>
-          <Input
-            label="Email"
-            placeholder="email@hmail.com"
-            onChangeText={(text) => this.onEmailChange(text)}
-          />
-        </CardSection>
-        <CardSection>
-          <Input secureTextEntry label="Password" placeholder="password" />
-        </CardSection>
-        <CardSection>
-          <Button>
-            Login
-          </Button>
-        </CardSection>
-      </Card>
-    );
-  }
+	onPasswordChange(text) {
+		this.props.passwordChanged(text);
+	}
+
+	onButtonPress() {
+		const { email, password } = this.props;
+		this.props.loginUser({ email, password });
+	}
+
+	render() {
+		return (
+            <Card>
+                <CardSection>
+                    <Input
+                        label="Email"
+                        placeholder="email@hmail.com"
+                        onChangeText={(text) => this.onEmailChange(text)}
+                        value={this.props.email}
+                    />
+                </CardSection>
+                <CardSection>
+                    <Input
+                        secureTextEntry
+                        label="Password"
+                        placeholder="password"
+                        onChangeText={(text) => this.onPasswordChange(text)}
+                    />
+                </CardSection>
+                <CardSection>
+                    <Button onPress={() => this.onButtonPress()}>
+                        Login
+                    </Button>
+                </CardSection>
+            </Card>
+		);
+	}
 }
 
-const mapStateToProps = (state) => {
-  return {
-    email: state.auth.email
-  };
-};
+    const mapStateToProps = state => (
+        {
+            email: state.auth.email,
+            password: state.auth.password
+        }
+    );
 
-export default connect(mapStateToProps, actions)(LoginForm);
+    export default connect(mapStateToProps, actions)(LoginForm);
+
